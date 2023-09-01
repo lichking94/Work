c8429cd8f091600d2484f6fd30bfee23d35cef86
Pablo Magdaleno <pablomagdaleno9410@gmail.com>
2023-09-01 12:59:53 -0400

    Modificacion de la tarjetas de dragones html creacion de array para insertar objetos en el doom y creacion de clase

--- js/mokepon.js
+++ js/mokepon.js
@@ -1,3 +1,50 @@
+
+
+// Primer conjunto de variables
+const reiniciarjuego = document.getElementById('reiniciar')
+const sectionSeleccionarAtaque =  document.getElementById('ataque')
+const botonMascota = document.getElementById("boton-mascota");
+const botonFuego = document.getElementById("boton-fuego");
+const botonTierra = document.getElementById("boton-tierra");
+const botonAgua = document.getElementById("boton-agua");
+const botonAire = document.getElementById("boton-aire");
+const botonReiniciar = document.getElementById('reiniciar')
+
+// Segundo conjunto de variablesconst
+const setcionSeleccionarMascota = document.getElementById('mascota')
+//let sectionSeleccionarAtaque =  document.getElementById('ataque')
+const animal1 = document.getElementById("caliope");
+const animal2 = document.getElementById("rocoso");
+const animal3 = document.getElementById("firedar");
+const animal4 = document.getElementById("vulcano");
+const animal5 = document.getElementById("pantano");
+const animal6 = document.getElementById("rocmag");
+const spanMascotaJugador = document.getElementById("mascota-jugador");
+
+//Tercer conjunto de variables
+const spanMascotaEnemigo = document.getElementById("mascota-enemigo");
+
+
+// Cuarto conjunto de variables
+const spanVidasJugador = document.getElementById("vida-jugador")
+const spanVidasEnemigo = document.getElementById("vida-enemigo")
+
+//Quinto conjunto de variables
+const sectionMensajes = document.getElementById('resultado')
+const ataqueDelJugador = document.getElementById('ataque-jugador')
+const ataquesDelEnemigo = document.getElementById('ataque-enemigo')
+const contenedorTarjetas = document.getElementById('contenedortarjetas')
+
+
+//Sexto conjunto de variables
+/*let sectionMensajes = document.getElementById('resultado')
+let botonFuego = document.getElementById("boton-fuego");
+let botonTierra = document.getElementById("boton-tierra");
+let botonAgua = document.getElementById("boton-agua");
+let botonAire = document.getElementById("boton-aire");
+let reiniciarjuego = document.getElementById('reiniciar')*/
+
+let dragones = []
 let ataqueJugador;
 let ataqueEnemigo;
 let resultado;
@@ -5,49 +52,99 @@ let vidasJugador = 6
 let vidasEnemigo = 6
 let victoria = 0
 let derrota = 0
-
-// Primer conjunto de variables
-let reiniciarjuego = document.getElementById('reiniciar')
-let sectionSeleccionarAtaque =  document.getElementById('ataque')
-let botonMascota = document.getElementById("boton-mascota");
-let botonFuego = document.getElementById("boton-fuego");
-let botonTierra = document.getElementById("boton-tierra");
-let botonAgua = document.getElementById("boton-agua");
-let botonAire = document.getElementById("boton-aire");
-let botonReiniciar = document.getElementById('reiniciar')
-
-// Segundo conjunto de variables
-
-let setcionSeleccionarMascota = document.getElementById('mascota')
-//let sectionSeleccionarAtaque =  document.getElementById('ataque')
-let animal1 = document.getElementById("caliope");
-let animal2 = document.getElementById("rocoso");
-let animal3 = document.getElementById("firedar");
-let animal4 = document.getElementById("vulcano");
-let animal5 = document.getElementById("pantano");
-let animal6 = document.getElementById("rocmag");
-let spanMascotaJugador = document.getElementById("mascota-jugador");
-
-//Tercer conjunto de variables
-let spanMascotaEnemigo = document.getElementById("mascota-enemigo");
+let opcionDragones 
 
 
-// Cuarto conjunto de variables
-let spanVidasJugador = document.getElementById("vida-jugador")
-let spanVidasEnemigo = document.getElementById("vida-enemigo")
+// creacion de clases 
 
-//Quinto conjunto de varialbes
-let sectionMensajes = document.getElementById('resultado')
-let ataqueDelJugador = document.getElementById('ataque-jugador')
-let ataquesDelEnemigo = document.getElementById('ataque-enemigo')
+class Dragones{
+    constructor(nombre, foto, vida){
+      this.nombre  = nombre
+      this.foto = foto
+      this.vida = vida
+      this.ataques = []
+    }
 
+
+}
+
+let caliope = new Dragones('Caliope', '/mokepon/assets/caliope.jpg', '6')
+let rocoso = new Dragones('Rocoso', '/mokepon/assets/rocoso.jpg', '6')
+let firedar = new Dragones('Firedar', '/mokepon/assets/firedar.jpg','6')
+let vulcano = new Dragones('Vulcano', '/mokepon/assets/vulcano.jpg','6')
+let pantano = new Dragones('Pantano', '/mokepon/assets/pantano.jpg','6')
+let rocmag = new Dragones('Rocmag', '/mokepon/assets/rocmag.jpg','6')
+
+
+caliope.ataques.push(
+  {nombre: '💧', id: 'boton-agua'},
+  {nombre: '💧', id: 'boton-agua'},
+  {nombre: '🌬️', id: 'boton-aire'},
+  {nombre: '🔥', id: 'boton-fuego'},
+  {nombre: '🪨', id: 'boton-tierra'},
+)
+
+rocoso.ataques.push(
+  {nombre: '🔥', id: 'boton-fuego'},
+  {nombre: '🔥', id: 'boton-fuego'},
+  {nombre: '🌬️', id: 'boton-aire'},
+  {nombre: '💧', id: 'boton-agua'},
+  {nombre: '🪨', id: 'boton-tierra'},
+)
+
+firedar.ataques.push(
+  {nombre: '🪨', id: 'boton-tierra'},
+  {nombre: '🪨', id: 'boton-tierra'},
+  {nombre: '🌬️', id: 'boton-aire'},
+  {nombre: '💧', id: 'boton-agua'},
+  {nombre: '🔥', id: 'boton-fuego'},
+)
+
+vulcano.ataques.push(
+  {nombre: '💧', id: 'boton-agua'},
+  {nombre: '💧', id: 'boton-agua'},
+  {nombre: '🌬️', id: 'boton-aire'},
+  {nombre: '🔥', id: 'boton-fuego'},
+  {nombre: '🪨', id: 'boton-tierra'},
+)
+pantano.ataques.push(
+  {nombre: '🔥', id: 'boton-fuego'},
+  {nombre: '🔥', id: 'boton-fuego'},
+  {nombre: '🌬️', id: 'boton-aire'},
+  {nombre: '💧', id: 'boton-agua'},
+  {nombre: '🪨', id: 'boton-tierra'},
+)
+rocmag.ataques.push(
+  {nombre: '🪨', id: 'boton-tierra'},
+  {nombre: '🪨', id: 'boton-tierra'},
+  {nombre: '🌬️', id: 'boton-aire'},
+  {nombre: '💧', id: 'boton-agua'},
+  {nombre: '🔥', id: 'boton-fuego'},
+)
+
+dragones.push(caliope,rocoso,firedar,vulcano,pantano,rocmag)
 
 
 //nos debemos llevar varialbes que se repitan 
+
+
 function iniciarJuego() {
     //let reiniciarjuego = document.getElementById('reiniciar') se repite
-    reiniciarjuego.style.display = 'none' 
+  reiniciarjuego.style.display = 'none' 
     
+  dragones.forEach((dragon) =>{
+      opcionDragones = `
+
+      <input type="radio" id="${dragon.nombre}" name="mascota" />
+      <label class="tarjeta-mascota"  for=${dragon.nombre}>
+        <p>${dragon.nombre}</p>
+        <img src=${dragon.foto} alt=${dragon.nombre}>
+      </label>
+      `
+      contenedorTarjetas.innerHTML += opcionDragones
+      console.log(contenedorTarjetas)
+  })
+
   //let sectionSeleccionarAtaque =  document.getElementById('ataque') se repite
   sectionSeleccionarAtaque.style.display = 'none'
 
@@ -179,8 +276,6 @@ function seleccionarMascotaEnemigo() {
   }
 }
 
-
-
 function ataqueFuego() {
   ataqueJugador = "FUEGO";
   ataqueAleatorioEnemigo();
@@ -299,26 +394,24 @@ function mensajesglobal(resultado){
 }
 
 function mensajeFinal(resultadoFinal){
-    let sectionMensajes = document.getElementById('resultado')
-
-    //let mensaje = document.createElement('p')
+    //let sectionMensajes = document.getElementById('resultado')
+    //let mensaje = document.createElement('p') Esta no se usa tiene un nombre que viejo no disposible
     sectionMensajes.innerHTML = resultadoFinal
-
     //sectionMensajes.appendChild(mensaje)
-    
-    let botonFuego = document.getElementById("boton-fuego");
+
+    //let botonFuego = document.getElementById("boton-fuego");
     botonFuego.disabled = true
 
-    let botonTierra = document.getElementById("boton-tierra");
+    //let botonTierra = document.getElementById("boton-tierra");//
     botonTierra.disabled = true
 
-    let botonAgua = document.getElementById("boton-agua");
+    //let botonAgua = document.getElementById("boton-agua");////
     botonAgua.disabled = true
 
-    let botonAire = document.getElementById("boton-aire");
+    //let botonAire = document.getElementById("boton-aire");//////
     botonAire.disabled = true
 
-    let reiniciarjuego = document.getElementById('reiniciar')
+   //let reiniciarjuego = document.getElementById('reinicia//////r')//
     reiniciarjuego.style.display = 'block'
 
 }
@@ -330,7 +423,5 @@ function recargarJuego(){
 function aleatorio(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
-
-
 
 window.addEventListener("load", iniciarJuego);
